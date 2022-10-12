31cdafcd230e1f907ae89079bf7dff949187296d
nchola <108863217+nchola@users.noreply.github.com>
2022-10-13 01:29:30 +0700

    PAB

--- app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
+++ app/src/main/java/com/si5a/implicitsi5a/MainActivity.java
@@ -9,34 +9,50 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.net.URI;
+
 public class MainActivity extends AppCompatActivity {
-    EditText etUrl, etLokasi, etTeks;
-    Button btnbukaWebsite, btnbukaLokasi, btnbagikanTeks;
-    String Url, Lokasi, Nama;
+    EditText etUrl,etLokasi,etTeks;
+    Button btnBukaWebsite, btnBukaLokasi, btnBagikanTeks;
+    String getWebsite,getLokasi, getTeks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUrl = findViewById(R.id.et_Url);
-        etLokasi = findViewById(R.id.etLokasi);
-        etTeks = findViewById(R.id.etTeks);
-        btnbukaWebsite = findViewById(R.id.btn_buka_website);
-        btnbukaLokasi = findViewById(R.id.btn_buka_lokasi);
-        btnbagikanTeks = findViewById(R.id.btn_bagikan_teks);
+        etUrl = findViewById(R.id.et_url);
+        etLokasi = findViewById(R.id.et_lokasi);
+        etTeks = findViewById(R.id.et_text);
+        btnBukaWebsite = findViewById(R.id.btn_buka_website);
+        btnBukaLokasi = findViewById(R.id.btn_buka_lokasi);
+        btnBagikanTeks = findViewById(R.id.btn_bagikan_teks);
 
-        btnbukaWebsite.setOnClickListener(new View.OnClickListener() {
+        btnBukaWebsite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String urlWebsite = etUrl.getText().toString();
-                Uri uriurlWebsite = Uri.parse(urlWebsite);
-                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, uriurlWebsite);
+                getWebsite = etUrl.getText().toString();
+                Uri urigetWebsite = Uri.parse(getWebsite);
+                Intent intentWebsite = new Intent(Intent.ACTION_VIEW, urigetWebsite);
                 try {
                     startActivity(intentWebsite);
                 }catch (Exception e){
-                    etUrl.setError("Url Tidak Sesuai");
+                    etUrl.setError("Url Tidak Sesuai!");
                 }
+            }
+        });
+
+        btnBukaLokasi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        btnBagikanTeks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
